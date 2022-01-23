13e4c2545970de92dbcb9b4e9fbfc46df895c035
mac Ray <rztechdude@yahoo.com>
2022-01-23 14:27:28 -0500

    start and end game functiond

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,21 +67,56 @@ var fight = function(enemyName) {
   }  
 
     } 
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
     for(var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0) {
-            // let player know what round they are in, remember that arrays start at 0 so it needs 1 added to it
+    // let player know what round they are in, remember that arrays start at 0 so it needs 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
-        }
-        //pick new enemy to fight based on the index of the enemyNames array
+        
+    //pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
     // reset enemtHealth before starting new fight
     enemyHealth = 50;
-
-    debugger;
     // pass the pickedEnemyName variable's value to the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    }
     else {
         window.alert("You have lost your robot in battle! ");
         break;
+        } endGame();
+    
+
+    // function to end the entire game
+    var endGame = function() {
+        // if a player is still alive, player wins
+        if (playerHealth> 0){
+            window.alert("Great job, you've syrvived the game! You now have a score of " + playerMoney + "."
+            );
         }
+        else {
+                window.alert("You've lost your robot in battle.");
+    
+        }
+      
     }
+    }
+    // ask player tif they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart the game
+        startGame();
+
+    }
+    else {
+        window.alert("Thank you for playing Robot Glaiators! Come back soon!"
+        );
+    }
+}
+startGame();
